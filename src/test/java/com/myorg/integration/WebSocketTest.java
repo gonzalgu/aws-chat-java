0b862754ba930d140f6251a07d1f4f21355708de
gonzalgu <gadol.gonzalez@gmail.com>
2024-09-11 10:49:47 -0400

    formatting tests

--- src/test/java/com/myorg/integration/WebSocketTest.java
+++ src/test/java/com/myorg/integration/WebSocketTest.java
@@ -25,20 +25,23 @@ import org.junit.jupiter.api.Test;
 public class WebSocketTest {
 
     /*
-gadol@DESKTOP-8F31RFM:~$ wscat -c  wss://swzxzc4mg0.execute-api.sa-east-1.amazonaws.com/dev
-Connected (press CTRL+C to quit)
-> {"action": "sendmessage", "message": "hello, everyone!"}
-< hello, everyone!
-> {"action": "sendmessage", "message": "hey there!"}
->
+     * gadol@DESKTOP-8F31RFM:~$ wscat -c
+     * wss://swzxzc4mg0.execute-api.sa-east-1.amazonaws.com/dev
+     * Connected (press CTRL+C to quit)
+     * > {"action": "sendmessage", "message": "hello, everyone!"}
+     * < hello, everyone!
+     * > {"action": "sendmessage", "message": "hey there!"}
+     * >
      * 
      */
 
-    static class WSListener implements WebSocket.Listener{
+    static class WSListener implements WebSocket.Listener {
         int id;
-        public WSListener(int index){
+
+        public WSListener(int index) {
             this.id = index;
         }
+
         @Override
         public void onOpen(WebSocket webSocket) {
             System.out.printf("[%d]: WebSocket opened\n", this.id);
@@ -49,37 +52,38 @@ Connected (press CTRL+C to quit)
         @Override
         public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
             // TODO Auto-generated method stub
-            System.out.printf("[%d]: Received message: %s\n", id,  data);
+            System.out.printf("[%d]: Received message: %s\n", id, data);
             return Listener.super.onText(webSocket, data, last);
         }
 
         @Override
         public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
             // TODO Auto-generated method stub
-            System.out.printf("[%d] WebSocket closed with statusCode: %s\n",id, statusCode);
+            System.out.printf("[%d] WebSocket closed with statusCode: %s\n", id, statusCode);
             return Listener.super.onClose(webSocket, statusCode, reason);
         }
 
         @Override
         public void onError(WebSocket webSocket, Throwable error) {
             // TODO Auto-generated method stub
-            //Listener.super.onError(webSocket, error);
-            System.err.printf("[%d] Error ocurrred: %s\n", id,  error.getMessage());
+            // Listener.super.onError(webSocket, error);
+            System.err.printf("[%d] Error ocurrred: %s\n", id, error.getMessage());
         }
     };
 
-    record Message(String action, String message){
+    record Message(String action, String message) {
         @Override
         public final String toString() {
             return String.format("{\"action\": \"%s\", \"message\": \"%s\"}", action, message);
         }
     };
-    Message makeMessage(String message){
+
+    Message makeMessage(String message) {
         return new Message("sendmessage", message);
     }
 
     @Test
-    void testPingPong() throws InterruptedException, ExecutionException{
+    void testPingPong() throws InterruptedException, ExecutionException {
         String chatApiURL = "wss://swzxzc4mg0.execute-api.sa-east-1.amazonaws.com/dev";
         URI uri = URI.create(chatApiURL);
         HttpClient client = HttpClient.newHttpClient();
@@ -88,15 +92,15 @@ Connected (press CTRL+C to quit)
         var ws = client.newWebSocketBuilder().buildAsync(uri, new WebSocket.Listener() {
             @Override
             public CompletionStage<?> onPong(WebSocket webSocket, ByteBuffer message) {
-                while(message.remaining() > 0){
+                while (message.remaining() > 0) {
                     byte b = message.get();
                     response.put(b);
                 }
-                pongReceived.set(true);                
+                pongReceived.set(true);
                 return Listener.super.onPong(webSocket, message);
             }
         }).join();
-        var dead_beef = new byte[]{0xD, 0xE, 0xA, 0xD, 0xB, 0xE, 0xE, 0xF};
+        var dead_beef = new byte[] { 0xD, 0xE, 0xA, 0xD, 0xB, 0xE, 0xE, 0xF };
         var buf = ByteBuffer.wrap(dead_beef);
         var fut = ws.sendPing(buf);
         fut.get();
@@ -105,38 +109,36 @@ Connected (press CTRL+C to quit)
         var arr = response.array();
         Assertions.assertArrayEquals(dead_beef, arr);
     }
-    
 
     @Test
-    void canSendTextBetweenSockets() throws InterruptedException, ExecutionException{
+    void canSendTextBetweenSockets() throws InterruptedException, ExecutionException {
         String chat = "wss://swzxzc4mg0.execute-api.sa-east-1.amazonaws.com/dev";
-        
+
         URI chatURI = URI.create(chat);
-        
+
         HttpClient client = HttpClient.newHttpClient();
         // Create 2 websocket instances
         var ws_f1 = client.newWebSocketBuilder().buildAsync(chatURI, new WSListener(1));
         var ws_f2 = client.newWebSocketBuilder().buildAsync(chatURI, new WSListener(2));
-         
-        var fut = CompletableFuture.allOf(ws_f1,ws_f2);
+
+        var fut = CompletableFuture.allOf(ws_f1, ws_f2);
         fut.join();
 
         var ws1 = ws_f1.get();
         var ws2 = ws_f2.get();
 
-        var msg1 = makeMessage("hey there!");       
+        var msg1 = makeMessage("hey there!");
         ws1.sendText(msg1.toString(), true);
         Thread.sleep(5000);
         var msg2 = makeMessage("Hallo neighbor!");
         ws2.sendText(msg2.toString(), true);
         Thread.sleep(5000);
         ws1.sendClose(WebSocket.NORMAL_CLOSURE, "Bye!")
-            .thenRun(() -> System.out.println("websocket closed")
-        );
+                .thenRun(() -> System.out.println("websocket closed"));
         Thread.sleep(2000);
         ws2.sendClose(WebSocket.NORMAL_CLOSURE, "K thx bye!")
-        .thenRun(() -> System.out.println("websocket closed"));
+                .thenRun(() -> System.out.println("websocket closed"));
 
-        Thread.sleep(1000);                    
+        Thread.sleep(1000);
     }
 }
